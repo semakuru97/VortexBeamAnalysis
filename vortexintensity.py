e928ced8accce1fdbcbbb746ec025da89875247c
Sema Kuru <32938361+semakuru97@users.noreply.github.com>
2019-03-11 09:50:05 +0000

    Add files via upload

--- vortexintensity.py
+++ vortexintensity.py
@@ -8,27 +8,25 @@ Created on Sat Feb  9 12:40:41 2019
 import numpy as np
 import pylab as pl
 import time
-#from mpl_toolkits import mplot3d
 
 def y_at_x(x):
-    y_at_x = np.zeros([height,1])
+    """ Function to collect all the y intensity data 
+    for a given x position on 2D graph, used to draw I vs y graph"""
+    y_at_x = np.zeros([height-1,1])
     for row in range(Matrix.shape[0]):
         y_at_x[row] = Matrix[row][x]
     return y_at_x
 
-height = 1088           # Needs to be set for each file
-width = 2048            # Needs to be set, given in info lines
-pixel = 5.5e-3
-
+size = 2
+height = 544 * size
+width = 1024 * size
+pixel = 0.011 / size
 x = 0                   # column index of the matrix set to zero
 y = 0                   # row index of the matrix set to zero
-
-before = time.time()
-with open("3z=50exp60.txt") as f:
+with open("vortex2.txt") as f:
     lines = f.readlines()           # Each line is y = y'
     info = lines[0:8]
-    Matrix = np.zeros([height,width])  
-    print(info)                     # Read what height and width is, and pixel size
+    Matrix = np.zeros([len(lines) - 9,width])   
     for i in lines[8:len(lines)-1]:
         a = ''                              # Data is in string, initialize each data a
         for j in i:
@@ -40,17 +38,17 @@ with open("3z=50exp60.txt") as f:
                 a = ''
         y = y + 1                           # Next row
         x = 0                               # Back to the first column
-after = time.time()
+print(info)
 # FIGURE
-print("time to read doc is ", after-before)
-x = np.arange(0,width,1) *pixel                  # x distance, multiply with physical length of a pixel
-y = np.arange(0,height,1)  *pixel       # y distance, multiply with physical length of a pixel
 
-Y = 2.2
-X = 5.7
+x = np.arange(0,width,1)*pixel                  # x distance, multiply with physical length of a pixel, 11um
+y = np.arange(0,len(lines) - 9,1)*pixel          # y distance, multiply with physical length of a pixel, 11um
 
-Y_pixel = int(Y *height /(height*pixel))
-X_pixel = int(X *width /(width*pixel))
+X = 2.6
+X_pixel = int(X*width/(width*pixel))
+
+Y = 1.5
+Y_pixel = int(Y*height/(height*pixel))
 
 before = time.time()
 
@@ -61,22 +59,22 @@ pl.xlabel("x (mm)")
 pl.ylabel("y (mm)")
 
 after = time.time()
-
 print("time to plot 2D is ", after - before)
-#
-#pl.figure(1 , figsize = (7,5))
-#
-#pl.plot(x , Matrix[Y_pixel])
-#pl.axis([X-1.5,X+1.5,0,1000])
-#pl.xlabel("x (mm)")
-#pl.ylabel("Intensity")
-#
-#pl.figure(3 , figsize = (7,5))
-#
-#pl.plot(y , y_at_x(X_pixel))
-#pl.axis([Y-1.5,Y+1.5,0,1000])
-#pl.xlabel("y (mm)")
-#pl.ylabel("Intensity")
+
+pl.figure(1 , figsize = (7,5))
+
+pl.plot(x , Matrix[Y_pixel])
+pl.axis([X-1.5,X+1.5,0,1000])
+pl.xlabel("x (mm)")
+pl.ylabel("Intensity")
+
+pl.figure(3 , figsize = (7,5))
+
+pl.plot(y , y_at_x(X_pixel))
+pl.axis([Y-1.5,Y+1.5,0,1000])
+pl.xlabel("y (mm)")
+pl.ylabel("Intensity")
+
 
 """ SECOND VORTEX """ 
 
@@ -204,6 +202,7 @@ print("time to plot 2D is ", after - before)
 #pl.xlabel("y (mm)")
 #pl.ylabel("Intensity")
 #pl.legend(["d = 85um, x = 6.17mm" , "d = 50um, x = 4.95mm", "d =150um, x = 6.20mm"])
+
 
 
 
